6b04cae21b2928ff8c900968e6c8c91743391cc5
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-20 16:47:14 -0500

    Changed homepage text and readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">Zack Brown</h1>
+<p>My friends call me Talls</p>
 
 <style lang="postcss">
   @reference "tailwindcss";
