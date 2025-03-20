63b113a0f7015531802d56928165857527ae0bec
zsbrown97 <51924558+zsbrown97@users.noreply.github.com>
2025-03-20 17:04:46 -0500

    moved background color to app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
-<h1 class="text-3xl font-bold underline">Zack Brown</h1>
-<p>My friends call me Talls</p>
-
-<style lang="postcss">
-  @reference "tailwindcss";
-  :global(html) {
-    background-color: theme(--color-gray-100);
-  }
-</style>
+<div class="text-center">
+  <h1 class="text-2xl font-bold underline">Zack Brown</h1>
+  <p>My friends call me Talls</p>
+</div>
